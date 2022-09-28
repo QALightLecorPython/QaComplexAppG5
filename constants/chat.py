0958ce5c8326a77a96a1fb13820c18c4c8f0cc77
QALightLecorPython <denkondrat13+1@gmail.com>
2022-09-28 21:48:26 +0300

    [Lesson 18] Add test for chat

---
+++ constants/chat.py
@@ -0,0 +1,3 @@
+class ChatConsts:
+    CHAT_INPUT_XPATH = ".//input[@id='chatField']"
+    CHAT_MESSAGES_XPATH = ".//div[@class='chat-self']"
--- constants/header.py
+++ constants/header.py
@@ -1,2 +1,3 @@
 class HeaderConsts:
     CREATE_POST_BUTTON_XPATH = ".//a[@href='/create-post']"
+    OPEN_CHAT_XPATH = ".//*[@data-icon='comment']"
